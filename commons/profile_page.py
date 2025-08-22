77f8103c98745507b045cc3cbde1583795436f82
ryanshang <ryan.d.shang@gmail.com>
2025-08-22 15:38:38 -0400

    refactor: use POM on test_save_info case

---
+++ commons/profile_page.py
@@ -0,0 +1,41 @@
+import time
+
+from commons.base_page import BasePage
+
+
+class SaveInfoPage(BasePage):
+    # 所有页面元素都定义为类属性
+    # 个人中心页面地址
+    url = "http://116.62.63.211/shop/user/index.html"
+    # 修改资料按钮
+    edit_profile_btn = "//a[contains(text(),'修改资料')]"
+    # 编辑按钮
+    edit_btn = "//a[contains(text(),'编辑')]"
+    # 昵称输入框元素
+    nickname_input = "//input[@placeholder='昵称']"
+    # 性别单选框
+    gender_radio = "/html/body/div[4]/div[3]/div/form/div[2]/div/label[3]/span"
+    # 日期选择(日历)
+    calendar_input = "//input[@placeholder='生日']"
+    # 确认按钮
+    save_btn = "//button[text()='保存']"
+
+    # 将用例执行步骤封装成实例方法
+    def submit(self, nickname: str, date: str):
+        self.find_element(self.edit_profile_btn).click()
+        self.find_element(self.edit_btn).click()
+        # 名字输入框
+        nickname_el = self.find_element(self.nickname_input)
+        # 清空名字
+        nickname_el.clear()
+        time.sleep(5)
+        # 输入名字
+        nickname_el.send_keys(nickname)
+        # 选择性别
+        self.find_element(self.gender_radio).click()
+        # 选择日期
+        self.find_element(self.calendar_input).send_keys(date)
+        # 点击确认按钮
+        self.find_element(self.save_btn).click()
+        # 返回实际结果
+        return self.get_message()
--- testcases/test_profile.py
+++ testcases/test_profile.py
@@ -7,6 +7,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
 from commons.funcs import login
+from commons.login_page import LoginInfoPage
+from commons.profile_page import SaveInfoPage
 
 
 @pytest.fixture(scope='class')
@@ -17,8 +19,11 @@ from commons.funcs import login
 def driver():
     d = webdriver.Chrome()
     d.maximize_window()
-    d.implicitly_wait(10)
-    msg = login(d, 'salewond', '123456')
+    # 发送请求访问项目地址 => 通过LoginInfoPage的类属性url访问
+    d.get(LoginInfoPage.url)
+    # 创建页面对象
+    login_page = LoginInfoPage(d)
+    msg = login_page.login('salewond', '123456')
     assert msg == "登录成功"
     yield d
     d.quit()
@@ -28,33 +33,9 @@ def driver():
 class TestProfile:
     # update and save user profile (info)
     def test_save_info(self, driver):
-        driver.get("http://116.62.63.211/shop/user/index.html")
-        # 点击"修改资料"按钮
-        driver.find_element(By.XPATH, "//a[contains(text(),'修改资料')]").click()
-        # 点击"编辑"按钮
-        driver.find_element(By.XPATH, "//a[contains(text(),'编辑')]").click()
-        # 保存昵称输入框元素
-        nickname_input = driver.find_element(By.XPATH, "//input[@placeholder='昵称']")
-        # 清空输入框的内容
-        nickname_input.clear()
-        # 清空后会弹出消息框“昵称2～16个字符之间” => 解决方案：等它消失
-        time.sleep(5)
-        # 输入新的内容
-        nickname_input.send_keys("吴彦祖")
-        # 选择性别
-        driver.find_element(By.XPATH, "/html/body/div[4]/div[3]/div/form/div[2]/div/label[3]/span").click()
-        calendar = driver.find_element(By.XPATH, "/html/body/div[4]/div[3]/div/form/div[3]/input")
-        # 清空并选择生日日期
-        calendar.clear()
-        calendar.send_keys("2025-08-20")
-        # 点击保存按钮
-        driver.find_element(By.XPATH, "/html/body/div[4]/div[3]/div/form/div[4]/button").click()
-        # 显式等待
-        wait = WebDriverWait(driver, 10)
-        # 使用匿名函数lambda
-        wait.until(lambda d: driver.find_element(By.XPATH, "//p[@class='prompt-msg']"))
-
-        msg = driver.find_element(By.XPATH, "//p[@class='prompt-msg']").text
+        driver.get(SaveInfoPage.url)
+        save_info_page = SaveInfoPage(driver)
+        msg = save_info_page.submit("张三", "2025-04-01")
         assert msg == "编辑成功"
 
     def test_user_avatar(self, driver):
