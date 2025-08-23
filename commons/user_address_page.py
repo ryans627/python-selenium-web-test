876fbce550bcf3525b7f76a028fe131eef4a8fc0
ryanshang <ryan.d.shang@gmail.com>
2025-08-23 08:58:11 -0400

    refactor: use POM to redesign test_add_address case

--- commons/user_address_page.py
+++ commons/user_address_page.py
@@ -1,4 +1,6 @@
 # 定义用户地址页面类
+import time
+
 from commons.base_page import BasePage
 
 
@@ -15,3 +17,43 @@ class UserAddressPage(BasePage):
         address_txt = self.find_element(self.address).text
         # 以tuple的形式返回
         return (user_name_txt, phone_txt, address_txt)
+
+class SaveUserAddressPage(BasePage):
+    url = "http://116.62.63.211/shop/useraddress/index.html"
+    # 新增地址按钮
+    add_btn = "//button[@data-popup-title='新增地址']"
+    name_input = "//input[@placeholder='姓名']"
+    phone_input = "//input[@placeholder='电话']"
+    # province
+    province_btn = "//span[text()='省份']"
+    province_option = "/html/body/div[1]/form/div[4]/div[1]/div/ul/li[4]"
+    # city
+    city_btn = "//span[text()='城市']"
+    city_option = "/html/body/div[1]/form/div[4]/div[2]/div/ul/li[2]"
+    # county
+    county_btn = "//span[text()='区/县']"
+    county_option = "/html/body/div[1]/form/div[4]/div[3]/div/ul/li[2]"
+    # detailed_address
+    detailed_address_input = "//*[@id='form-address']"
+    # save button
+    save_btn = "//button[text()='保存']"
+
+    def add_address(self, name, phone, detailed_address):
+        self.find_element(self.add_btn).click()
+        # 切换iframe的方法已经在BasePage中封装好了，直接调用即可
+        self.iframe()
+        self.find_element(self.name_input).send_keys(name)
+        self.find_element(self.phone_input).send_keys(phone)
+
+        self.find_element(self.province_btn).click()
+        self.find_element(self.province_option).click()
+
+        self.find_element(self.city_btn).click()
+        self.find_element(self.city_option).click()
+
+        self.find_element(self.county_btn).click()
+        self.find_element(self.county_option).click()
+
+        self.find_element(self.detailed_address_input).send_keys(detailed_address)
+        self.find_element(self.save_btn).click()
+        return self.get_message()
