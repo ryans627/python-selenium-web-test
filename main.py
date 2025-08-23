d38cf27dae5a79840a81e29568d671b72d037c95
ryanshang <ryan.d.shang@gmail.com>
2025-08-23 10:05:57 -0400

    feature: integrate with allure report

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import pytest
+import os
 
-pytest.main(['-vsk', 'test_profile'])
+# 项目主程序入口
+# 默认运行所有测试用例
+pytest.main(['-vsk', '--clean-alluredir', '--alluredir', './allure_result'])
+
+# 清理旧的测试报告，将allure的测试结果分析生成HTML报告
+os.system('allure generate ./allure_result -o ./report --clean')
