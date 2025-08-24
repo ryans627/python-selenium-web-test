d494b7fcf8200c3aa50af949360ac122b0c0cc62
ryanshang <ryan.d.shang@gmail.com>
2025-08-24 11:57:40 -0400

    removed os operation

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import os
 
 # 项目主程序入口
 # 默认运行所有测试用例
-pytest.main(['-vs', '--clean-alluredir', '--alluredir', './allure_result'])
+pytest.main(['-vs', '--clean-alluredir', '--alluredir', './allure-results'])
 
 # 清理旧的测试报告，将allure的测试结果分析生成HTML报告
-os.system('allure generate ./allure_result -o ./report --clean')
+# 由于我们使用Jenkins集成，可以注释掉本地os操作
+# os.system('allure generate ./allure-results -o ./report --clean')
